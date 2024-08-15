c13656ef5ebd4b3a6703d6267a78aa8a4a54e52f
Vinay Saddanapu <chintu2030@gmail.com>
2024-08-15 23:28:33 +0530

    1. VoteEligiblity&Divisiblity5And7

---
+++ src/main/scala/Divisiblity5And7.scala
@@ -0,0 +1,9 @@
+object Divisiblity5And7 {
+  private def checkDivisiblity5And7(num: Int): Boolean = {
+     (num%5 == 0 && num%7 == 0)
+  }
+
+  def main(args: Array[String]): Unit = {
+    println("Check Divisibility by 5 and 7: "+ checkDivisiblity5And7(35))
+  }
+}
---
+++ src/main/scala/VoteEligibility.scala
@@ -0,0 +1,17 @@
+object VoteEligibility {
+  def checkEligiblity(age: Int): String = {
+    if(age >= 18) {
+      "Eligible to vote"
+    } else if(age >= 16) {
+      "Eligible to drive"
+    }else{
+      "Not Eligible"
+    }
+  }
+
+  def main(args: Array[String]): Unit = {
+    println("Age 18: "+ checkEligiblity(20))
+    println("Age 16: "+ checkEligiblity(16))
+
+  }
+}
